d793a293ef6c2919ff461488e12662061974edbc
RoyalWeaboo <malikazizali86@gmail.com>
2022-08-19 22:02:09 +0700

    Try second commit

--- app/src/main/java/com/malikazizali/helloworldkotlin/MainActivity.kt
+++ app/src/main/java/com/malikazizali/helloworldkotlin/MainActivity.kt
@@ -7,9 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        funNew()
     }
 
     fun funNew(){
-        var angkaSatu : Int
+        val angkaSatu : Int = 1
     }
 }
