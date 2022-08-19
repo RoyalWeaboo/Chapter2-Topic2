8b670922d7a36e356f69c2bb2ba52003a6737cee
RoyalWeaboo <malikazizali86@gmail.com>
2022-08-19 21:12:38 +0700

    first time commit

---
+++ app/src/main/java/com/malikazizali/helloworldkotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.malikazizali.helloworldkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun funNew(){
+        var angkaSatu : Int
+    }
+}
